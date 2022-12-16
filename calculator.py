c5f21d10cf0be24bd6a01b104b69f4d7ca90ad85
Evgeniy Shabolin <49404135+Kitrop@users.noreply.github.com>
2022-12-17 00:12:47 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -150,7 +150,7 @@ class Calculator(QWidget):
 
             if self.op == "/" and self.num_1 != 0 and self.num_2 != 0:
                 self.input.setText(str(self.num_1 / self.num_2))
-            else:
+            else self.num_1 == 0 or self.num_2 == 0:
                 return self.input.setText("Нельзя делить на ноль")
 
 
